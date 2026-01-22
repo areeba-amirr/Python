dc9389d8b6114387659e7fac9bf9a81c8a5cb32c
areeba-amirr <areebaamir826@gmail.com>
2026-01-22 19:12:36 +0500

    Variables

---
+++ variable.py
@@ -0,0 +1,17 @@
+a = 1
+b= 1
+print(a+b)
+
+# Valid Identifiers
+num = 9
+_num = 9
+num1 = 9
+print(num)
+print(_num)
+print(num1)
+
+#Invalid Idenrtifiers
+# num 1 = 9
+# 1num = 9
+# @num = 9
+# if = 4
