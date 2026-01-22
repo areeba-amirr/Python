a694808e58c7a62c69598648e3cdb56ce93c605a
areeba-amirr <areebaamir826@gmail.com>
2026-01-22 19:13:14 +0500

    Data Types and type()

---
+++ dataTypes.py
@@ -0,0 +1,18 @@
+#Integer
+no1 = 3
+no2 = 7
+#Floating Point
+num1 = 2.33
+num2 = 999.33
+#String
+name = "Areeba"
+me = 'Areeba'
+#Boolena  (True or False)
+ans = True
+ans = False
+#None
+var = None
+print(type(var)) #None Type
+print(type(me))
+print(type(num2))
+print(type(no1))
