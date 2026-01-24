51b63b834cc392931ad13f23923fbd46013fdbcc
areeba-amirr <areebaamir826@gmail.com>
2026-01-24 21:16:28 +0500

    Practice Questions

---
+++ practice.py
@@ -0,0 +1,11 @@
+#add to number
+no1 = int(input("Enter first number : "))
+no2 = int(input("Enter sceond number : "))
+print(no1+no2)
+avg = (no1+no2)/2
+print(avg)
+#square
+print(no1*no1)
+#cube
+print(no1*no1*no1)
+
