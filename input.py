cd9bf9ec2686d0c2362078bdc11e504ce9c4fbba
areeba-amirr <areebaamir826@gmail.com>
2026-01-24 21:15:54 +0500

    Input function

---
+++ input.py
@@ -0,0 +1,13 @@
+# num1 = input("Enter num1 : ")
+# num2 = input("Enter num2 : ")
+# print("Num 1 : ",num1, " Num 2 : ",num2)
+# print(num1+num2)
+
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+print("Num 1 : ",num1, " Num 2 : ",num2)
+print(num1+num2)
+
+name = input("Enter your name : ")
+fName = input("Enter you father name : ")
+print("Hello , " +name+fName)
