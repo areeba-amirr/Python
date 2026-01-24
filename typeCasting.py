5b494a0b15302d38e5ed040d6e7541f0e8a520e6
areeba-amirr <areebaamir826@gmail.com>
2026-01-24 21:15:21 +0500

    Type Casting

---
+++ typeCasting.py
@@ -0,0 +1,17 @@
+# str() , int() , float()
+no = "44.55"
+print(type(no))
+newType = float(no)
+print(type(newType))
+
+num = 99
+print(type(num))
+print(type(float(num)))
+print(float(num))
+num = float(num)
+print(num)
+
+print(type(str(num)))
+# Invalid
+# name = "Areeba"
+# print(type(int(name)))
