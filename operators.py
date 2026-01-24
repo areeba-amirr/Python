a81edbf97e5120d6554db087650152897e7894a5
areeba-amirr <areebaamir826@gmail.com>
2026-01-24 21:14:48 +0500

    Operators

---
+++ operators.py
@@ -0,0 +1,35 @@
+# Arithmetic Operator
+a = 20
+b = 30
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+# Assignment Operator
+a = b
+print(a)
+a += 20
+print(a)
+a -= b
+print(a)
+a *= 3
+print(a)
+
+# Comperator Operator
+print(a>b)
+print(b>a)
+print(a>=b)
+print(a<=b)
+print(a!=b)
+print(a==b)
+
+# Logical Operator
+print(True and True)
+print(True and False)
+print(False and False)
+print(True or True)
+print(True or False)
+print(not True)
+print(not False)
