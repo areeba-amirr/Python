75d99bb95e2ebcc81984207d693b645b89ab9b4a
areeba-amirr <areebaamir826@gmail.com>
2026-01-25 18:08:53 +0500

    Practice sets

--- practice.py
+++ practice.py
@@ -1,11 +1,28 @@
-#add to number
-no1 = int(input("Enter first number : "))
-no2 = int(input("Enter sceond number : "))
-print(no1+no2)
-avg = (no1+no2)/2
-print(avg)
-#square
-print(no1*no1)
-#cube
-print(no1*no1*no1)
+# #add to number
+# no1 = int(input("Enter first number : "))
+# no2 = int(input("Enter sceond number : "))
+# print(no1+no2)
+# avg = (no1+no2)/2
+# print(avg)
+# #square
+# print(no1*no1)
+# #cube
+# print(no1*no1*no1)
+#Replace double spacr to single space
+str = " A  B cfsuhushc  uidcih"
+strNew = str.find("  ")
+print(strNew)
+print(str.replace("  "," "))
+# print(str[strNew].replace("  "," ")) doesnot work bcz this is not string it has only one chaarcter
+# uInput = input("Enter any thing:")
+# #lowercase user input
+# print(uInput.lower())
+# #check if input conatin letters+digit
+# print(uInput.isalnum())
+# #Trim extra space
+# print(uInput.strip())
+# #length
+# print(len(uInput))
 
+# str = "Dear <your name> , you are invited on <day>"
+# print(str.replace("<your name>","Areeba").replace("<day>","Monday"))
