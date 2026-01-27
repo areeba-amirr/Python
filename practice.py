3b5d4d0ccd10f6565484acc7e0f6d51c633d195c
areeba-amirr <areebaamir826@gmail.com>
2026-01-27 22:16:00 +0500

    Practice Question

--- practice.py
+++ practice.py
@@ -9,10 +9,10 @@
 # #cube
 # print(no1*no1*no1)
 #Replace double spacr to single space
-str = " A  B cfsuhushc  uidcih"
-strNew = str.find("  ")
-print(strNew)
-print(str.replace("  "," "))
+# str = " A  B cfsuhushc  uidcih"
+# strNew = str.find("  ")
+# print(strNew)
+# print(str.replace("  "," "))
 # print(str[strNew].replace("  "," ")) doesnot work bcz this is not string it has only one chaarcter
 # uInput = input("Enter any thing:")
 # #lowercase user input
@@ -26,3 +26,52 @@ print(str.replace("  "," "))
 
 # str = "Dear <your name> , you are invited on <day>"
 # print(str.replace("<your name>","Areeba").replace("<day>","Monday"))
+#store 7 fruits in a list by user input
+# fruits = []
+# fruits.append(input("Enter Frist Fruit : "))
+# fruits.append(input("Enter Second Fruit : "))
+# fruits.append( input("Enter Third Fruit : "))
+# fruits.append (input("Enter Fourth Fruit : "))
+# fruits.append (input("Enter Fifth Fruit : "))
+# fruits.append (input("Enter Sixth Fruit : "))
+# fruits.append (input("Enter Seventh Fruit : "))
+# print(fruits)
+ #or
+# fruits = [None] * 7
+
+# fruits[0] = input("Enter First Fruit : ")
+# fruits[1] = input("Enter Second Fruit : ")
+# fruits[2] = input("Enter Third Fruit : ")
+# fruits[3] = input("Enter Fourth Fruit : ")
+# fruits[4] = input("Enter Fifth Fruit : ")
+# fruits[5] = input("Enter Sixth Fruit : ")
+# fruits[6] = input("Enter Seventh Fruit : ")
+
+# print(fruits)
+
+#student marks
+# marks = [None] * 6
+# marks[0] = float(input("Enter Your marks : "))
+# marks[1] = float(input("Enter Your marks : "))
+# marks[2] = float(input("Enter Your marks : "))
+# marks[3] = float(input("Enter Your marks : "))
+# marks[4] = float(input("Enter Your marks : "))
+# marks[5] = float(input("Enter Your marks : "))
+# marks.sort()
+# print(marks)
+#sum list
+# sum = 0
+list1 = [2,5,6,7]
+# sum += list1[0]
+# sum += list1[1]
+# sum += list1[2]
+# sum += list1[3]
+# print(sum)
+#or
+# print(sum(list1))
+#count no of 0's in tuple
+# t1 = (2,4,0,0,8,0,6,9,0)
+# print(t1.count(0))
+# t1 = (22,3,4,6)
+# str(t1)
+# print(type(t1))
